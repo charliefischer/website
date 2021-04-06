fc20b661c7892f704fcb36051aee9e8da9342b2a
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-04-06 20:05:46 +0100

    images added

--- components/illustrationFeed.js
+++ components/illustrationFeed.js
@@ -1,4 +1,5 @@
 import styles from './illustrationFeed.module.css'
+import Image from 'next/image'
 
 export default function illustrationFeed() {
   return(
@@ -6,6 +7,54 @@ export default function illustrationFeed() {
       <h1>
         Illustrator
       </h1>
+
+      <div className={styles.post}>
+        <Image 
+        src='/illustration/learn_to_touch.png'
+        alt='FreeWord'
+        width={300}
+        height={300}
+        layout='responsive'
+        objectFit='cover'
+        objectPosition='50% 0%'
+        />
+      </div>
+
+      <div className={styles.post}>
+        <Image 
+        src='/illustration/miles_runs.png'
+        alt='Miles Runs the Voodoo down'
+        width={300}
+        height={300}
+        layout='responsive'
+        objectFit='cover'
+        objectPosition='50% 0%'
+        />
+      </div>
+      
+      <div className={styles.post}>
+        <Image 
+        src='/illustration/riot_wrap.jpg'
+        alt='Riot Chicken'
+        width={300}
+        height={300}
+        layout='responsive'
+        objectFit='cover'
+        objectPosition='50% 0%'
+        />
+      </div>
+      
+      <div className={styles.post}>
+        <Image 
+        src='/illustration/lpgd.jpg'
+        alt='Labour Party Graphic Design'
+        width={300}
+        height={300}
+        layout='responsive'
+        objectFit='cover'
+        objectPosition='50% 0%'
+        />
+      </div>
     </div>
   )
 }
--- components/webdevFeed.js
+++ components/webdevFeed.js
@@ -1,8 +1,46 @@
 import styles from './webdevFeed.module.css'
+import Image from 'next/image'
 
-export default function() {
+
+export default function webdevFeed() {
   return(
     <div className={styles.feed}>
+      <div className={styles.post}>
+        <Image 
+        src='/web/type_checker.png'
+        alt='Type Checker'
+        width={300}
+        height={200}
+        layout='responsive'
+        objectFit='cover'
+        objectPosition='50% 0%'
+        />
+      </div>
+
+      <div className={styles.post}>
+        <Image 
+        src='/web/in_looking_out.png'
+        alt='In Looking Out'
+        width={300}
+        height={200}
+        layout='responsive'
+        objectFit='cover'
+        objectPosition='50% 0%'
+        />
+      </div>
+
+      <div className={styles.post}>
+        <Image 
+        src='/web/BBB.png'
+        alt='Facebook Clone'
+        width={300}
+        height={200}
+        layout='responsive'
+        objectFit='cover'
+        objectPosition='50% 0%'
+        />
+      </div>
+
       <h1>
         Web Developer
       </h1>
