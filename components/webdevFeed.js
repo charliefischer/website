0d0a164e5908bf53862d37f224165a7828b3a063
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-04-16 14:16:02 +0100

    updated hover states on index

--- components/webdevFeed.js
+++ components/webdevFeed.js
@@ -10,43 +10,53 @@ export default function webdevFeed() {
       <div className={styles.webfeed}>
         <div className={styles.rotate}>
             <div className={styles.post}>
+              <h2 >BBB</h2>
               <a href='https://github.com/charliefischer/acebook-BBB' target="_blank">
                 <img src='/web/BBB.png' alt='Facebook Clone' />
               </a>
-                <h2 >BBB</h2>
+              <div className={styles.hoverText}>
                 <p >A Facebook clone built using Ruby on Rails and tested with Rspec</p>
+              </div>
             </div>
 
             <div className={styles.post}>
+              <h2>Xtreme BnB</h2>
               <a href='https://github.com/charliefischer/Xtreme_Prestige_Worldwide_BnB' target="_blank">
                 <img src='/web/xtremebnb.png' alt='Air BnB Clone' />
               </a>
-                <h2>Xtreme BnB</h2>
+              <div className={styles.hoverText}>
                 <p >An AirBnB clone built in an MVC framework with Ruby and tested in Rspec</p>
+              </div>
             </div>
 
             <div className={styles.post}>
+              <h2>In Looking Out</h2>
               <a href='http://www.inlookingout.co.uk/' target="_blank">
                 <img src='/web/in_looking_out.png' alt='In Looking Out' />
               </a>
-                <h2>In Looking Out</h2>
+              <div className={styles.hoverText}>
                 <p>An online art gallery displaying works of art made during lockdown. Built using HTML, css and javascript</p>
+              </div>
             </div>
 
             <div className={styles.post}>
+              <h2>Type Checker</h2>
               <a href='https://cf-type-checker-draft.superhi.com/' target="_blank">
                 <img src='/web/type_checker.png' alt='Type Checker' loader='lazy' />
               </a>
-                <h2>Type Checker</h2>
+              <div className={styles.hoverText}>
                 <p>A SuperHi tutorial learning how to use form tags such as inputs, sliders, checkboxes and select boxes to build up interactivity to our projects.</p>
+              </div>
             </div>
             
             <div className={styles.post}>
+              <h2>Words of Women</h2>
               <a href='https://cf-words-of-women-draft.superhi.com/' target="_blank">
                 <img src='/web/words_of_women.png' alt='Words of Women' />
               </a>
-               <h2>Words of Women</h2>
+              <div className={styles.hoverText}>
                 <p>A SuperHi tutorial learning how to add parallax movement and stamps with vanilla javascript</p>
+              </div>
             </div>
 
         </div>
--- pages/illustration/inLookingOut.js
+++ pages/illustration/inLookingOut.js
@@ -20,6 +20,13 @@ export default function milesDavis(){
             artists sent in their illustrations and soundscapes to present their feelings in the COVID lockdowns. The final site
             I created can be seen  <a href='http://www.inlookingout.co.uk/' target='_blank'>here</a>
           </p>
+          <p>
+            My desk faces out the front of my house so I've taken quite a love for neighbourhood watch. 
+            Although I can see everyone is just sat in their front rooms watching TV all day, I 
+            like to imagine what the weirdest scenario I could spot through someones window 
+            if they think no one is watching. Re-enacting the Patrick Swayze Ghost scene with 
+            their teddy bear? Maybe.
+          </p>
         </div>
       <div className={styles.body}>
       <div className={styles.mainImage}>
