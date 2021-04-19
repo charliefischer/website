a9e8b2103cf73d45caf30886ac454640a713c62f
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-04-19 14:49:07 +0100

    layout looking 👍

--- components/navbar.js
+++ components/navbar.js
@@ -4,12 +4,14 @@ import styles from './navbar.module.css'
 export default function navbar() {
   return (
     <div className={styles.nav}>
-      <Link href='/'>
-        <a className={styles.name}>Charlie Fischer</a>
-      </Link>
-      <Link href='/about'>
-      <a className={styles.about}>About Me</a>
-      </Link>
+      <div className={styles.flex}>
+        <Link href='/'>
+          <a className={styles.name}>Charlie Fischer</a>
+        </Link>
+        <Link href='/about'>
+        <a className={styles.about}>About Me</a>
+        </Link>
+      </div>
     </div>
   )
 }
