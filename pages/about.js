5ac3742eba423afb3155a0abab8cd4a5aeeb7a1f
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-04-19 20:17:25 +0100

    amended about page

--- pages/about.js
+++ pages/about.js
@@ -12,9 +12,7 @@ export default function about() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.text}>
-        <div className={styles.title}>
-          About Me
-        </div>
+        <h1 className={styles.title}>About Me</h1>
         <div className={styles.content}>
           <p>
             My name’s Charlie, I’m a junior full stack web developer and freelance illustrator with a love for observational humour. I recently completed a four month coding bootcamp at Makers Academy where I taught myself Javascript and Ruby by building a range of applications and websites. In the process I gained a sound understanding of OOP, Agile methodologies and test driven development. What excites me most about web development is applying my creative thinking to problem solving; attacking any challenge from different angles and thinking of new ways around a problem as well as providing clean and fresh designed user interfaces.
@@ -36,7 +34,7 @@ export default function about() {
           </div>
         </div>
         </div>
-        <div className={styles.image}>
+        <div className={styles.images}>
           <img className={styles.brain} src='/about/brain.jpg' alt='Self Portrait' />
         </div>
     </Layout>
